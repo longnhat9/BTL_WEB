a3158adf85ff9e0d7a015a6188be7c40474b2109
longnhat9 <longnhatstar2002@gmail.com>
2022-11-03 11:41:06 +0700

    upload

---
+++ javascript/account.js
@@ -0,0 +1,27 @@
+// Begin Account
+
+getForm_login = document.querySelector('.form-login');
+getForm_register = document.querySelector('.form-register');
+
+getHeader_login = document.querySelector('.form-login__heading-login span');
+getHeader_register = document.querySelector('.form-login__heading-register span');
+
+getHeader_login.onclick = function() {
+    getForm_register.style.display = 'none';
+    getForm_login.style.display = 'flex';
+    getForm_login.style.flexDirection = 'center';
+}
+
+getHeader_register.onclick = function() {
+    getForm_login.style.display = 'none';
+    getForm_register.style.display = 'flex';
+    getForm_register.style.flexDirection = 'center';
+}
+
+// End Account
+
+// Begin Product
+
+
+
+// End Product
